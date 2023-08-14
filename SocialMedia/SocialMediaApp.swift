0d73d28a2a17bad7e6ede9a7c11f057dd0ac1c2f
eyhdev <alaqsa335@gmail.com>
2023-08-14 18:59:44 +0300

    Initial Commit

---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by eyh.mac on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
